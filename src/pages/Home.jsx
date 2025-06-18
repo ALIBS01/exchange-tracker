af878a9a03a2b335d6c1f30603748d9ea49e0233
Ali Baş <alibas2001@gmail.com>
2025-06-18 14:20:57 +0300

    Import globalstats

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import CryptoList from "../components/CryptoList";
 import TrendingList from "../components/TrendingList";
 import AnalyticsDashboard from "../components/AnalyticsDashboard";
+import GlobalStats from "../components/GlobalStats";
 
 const Home = () => {
   const [selectedCoin, setSelectedCoin] = useState("bitcoin");
 
   return (
     <div className="p-4">
+      <GlobalStats />
       <AnalyticsDashboard />
       <TrendingList onCoinSelect={setSelectedCoin} />
       <CryptoList onCurrencySelect={setSelectedCoin} />
