33cb4397a77116e2e8013fa0a2cce7e6dfb53c44
Ali Baş <alibas2001@gmail.com>
2025-06-26 16:08:30 +0300

    feat: Improve layout

--- src/pages/Highlights.jsx
+++ src/pages/Highlights.jsx
@@ -1,29 +1,26 @@
-import TrendingCoins from "../components/TrendingCoins";
 import TopGainers from "../components/TopGainers";
 import TopLosers from "../components/TopLosers";
 import NewlyAddedCoins from "../components/NewlyAddedCoins";
 import HighestVolumeCoins from "../components/HighestVolumeCoins";
+import TrendingCoins from "../components/TrendingCoins";
 import PriceChangeATH from "../components/PriceChangeATH";
 
 const Highlights = () => {
   return (
-    <div className="p-4 space-y-8">
-      <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">Highlights</h2>
-
-      <TrendingCoins />
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <section className="p-4 space-y-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <TopGainers />
         <TopLosers />
       </div>
-
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <NewlyAddedCoins />
         <HighestVolumeCoins />
       </div>
-
-      <PriceChangeATH />
-    </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <TrendingCoins />
+        <PriceChangeATH />
+      </div>
+    </section>
   );
 };
 
